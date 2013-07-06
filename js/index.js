b686185f05620e8dfc02be14e373008c2ad55b5c
Krishna Chaitanya <bkchaitan94@gmail.com>
2013-07-06 18:18:55 +0530

    Added Sriram's footer and index.html changes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	//FB, Twitter, gplus logos mouse hover events
-	$("#fblogo").mouseenter(function(){$(this).animate({width: '50px', height: '50px'}, 200);});
-	$("#fblogo").mouseleave(function(){$(this).animate({width: '30px', height: '30px'}, 200);});
-	$("#gpluslogo").mouseenter(function(){$(this).animate({width: '50px', height: '50px'}, 200);});
-	$("#gpluslogo").mouseleave(function(){$(this).animate({width: '30px', height: '30px'}, 200);});
-	$("#youtubelogo").mouseenter(function(){$(this).animate({width: '50px', height: '50px'}, 200);});
-	$("#youtubelogo").mouseleave(function(){$(this).animate({width: '30px', height: '30px'}, 200);});
+	$("#fblogo").mouseenter(function(){$(this).attr('src','images/Logos/fb_logo2.png').animate(1000);});
+	$("#fblogo").mouseleave(function(){$(this).attr('src','images/Logos/fb_logo.png').animate(1000);});
+	$("#gpluslogo").mouseenter(function(){$(this).attr('src','images/Logos/gplus_logo2.png').animate(1000);});
+	$("#gpluslogo").mouseleave(function(){$(this).attr('src','images/Logos/gplus_logo.png').animate(1000);});
+	$("#youtubelogo").mouseenter(function(){$(this).attr('src','images/Logos/youtube_logo2.png').animate(1000);});
+	$("#youtubelogo").mouseleave(function(){$(this).attr('src','images/Logos/youtube_logo.png').animate(1000);});
 });
